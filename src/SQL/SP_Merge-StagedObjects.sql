50935e68ee9cde300058e03af73650e8dcf9c3a8
Fredrik Melby <fredrik_melby@yahoo.no>
2019-03-04 14:53:19 +0100

    Update SP_Merge-StagedObjects.sql
    Fixed error when updating Objects table with staged data (NULL values in AccountName/DisplayName)

--- src/SQL/SP_Merge-StagedObjects.sql
+++ src/SQL/SP_Merge-StagedObjects.sql
@@ -1,23 +1,18 @@
 USE [MIM_RequestArchive]
 GO
-
-/****** Object:  StoredProcedure [dbo].[Merge-StagedObjects]    Script Date: 09/20/2016 09:51:52 ******/
+/****** Object:  StoredProcedure [dbo].[Merge-StagedObjects]    Script Date: 3/4/2019 1:51:07 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-
-
 
 -- =============================================
 -- Author:		Fredrik Melby
 -- Create date: 20.11.2015
 -- Description:	Merges Staging_Objecs to Objects
--- Updated: 18.3.2016
+-- Updated: 4.3.2019
 -- =============================================
-CREATE PROCEDURE [dbo].[Merge-StagedObjects] 
+ALTER PROCEDURE [dbo].[Merge-StagedObjects] 
 
 AS
 BEGIN
@@ -39,15 +34,13 @@ WHEN NOT MATCHED BY SOURCE AND Destination.Deleted IS NULL THEN UPDATE
 		Destination.MarkedAsDeleted = Getdate()
 	
 -- Update existing object with new DisplayName or AccountName
-WHEN MATCHED AND Destination.DisplayName != Source.DisplayName OR Destination.AccountName != Source.AccountName THEN UPDATE
+WHEN MATCHED AND Destination.DisplayName != Source.DisplayName 
+	OR Destination.AccountName != Source.AccountName 
+	OR (Destination.AccountName IS NULL AND Source.AccountName IS NOT NULL)
+	OR (Destination.DisplayName IS NULL AND Source.DisplayName IS NOT NULL)
+	THEN UPDATE
+	
 	SET Destination.DisplayName = Source.DisplayName,
 		Destination.AccountName = Source.AccountName;
 		
 END
-
-
-
-
-GO
-
-
